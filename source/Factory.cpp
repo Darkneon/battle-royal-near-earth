86f228d2227c96eb2b5cb733b026d00b32f87701
Jonathan Bergeron <jonathan.bergeron@hotmail.com>
2012-02-02 14:20:13 -0500

    Added Factory.h and Factory.cpp

---
+++ source/Factory.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   Factory.cpp
+ * Author: Addisson
+ * 
+ * Created on February 2, 2012, 2:19 PM
+ */
+
+#include "Factory.h"
+#include "FactoryModel.h"
+
+Factory::Factory() {
+    model = (Model*)(new FactoryModel);
+    
+}
+
+Factory::~Factory() {
+    if (model != NULL) {
+		delete model;
+		model = NULL;
+	}
+}
+
+void Factory::draw() {
+    model->draw();
+}
