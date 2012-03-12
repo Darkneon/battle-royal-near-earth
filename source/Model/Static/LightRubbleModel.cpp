e345dec5d6271f9a79bc59586522b2683278f7d5
Stefanie Lavoie <lavoie.stefanie@gmail.com>
2012-03-12 16:59:25 -0400

    Started texture on Stef's part. Fix help menu

--- source/Model/Static/LightRubbleModel.cpp
+++ source/Model/Static/LightRubbleModel.cpp
@@ -20,6 +20,7 @@ LightRubbleModel::LightRubbleModel(void)
 
 void LightRubbleModel::render() {
 	glColor3f(.7f,.7f,.7f);
+	glEnable(GL_TEXTURE_2D);
 	glPushMatrix();
 		glTranslatef(0.15f,0.0f,0.2f);
 			drawRock();
@@ -58,25 +59,32 @@ void LightRubbleModel::render() {
 			drawRock();
 		glTranslatef(-0.65f, 0.0f, -0.7f);
 	glPopMatrix();
+	glDisable(GL_TEXTURE_2D);
 }
 
 void LightRubbleModel::drawRock() {
 	glPushMatrix();
 		glScalef(0.15f,0.15f,0.2f);
-        GLUquadric *quadratic=gluNewQuadric();
-		gluQuadricNormals(quadratic, GLU_SMOOTH);
-		gluSphere(quadratic,0.5,10,10);
-		gluDeleteQuadric(quadratic);
+        GLUquadric *qRock=gluNewQuadric();
+		gluQuadricNormals(qRock, GLU_SMOOTH);
+		glBindTexture(GL_TEXTURE_2D, TextureManager::getInstance()->getTextures("rusted_zinc.bmp"));
+		gluQuadricTexture(qRock,true);
+
+		gluSphere(qRock,0.5,10,10);
+		gluDeleteQuadric(qRock);
 	glPopMatrix();
 }
 
 void LightRubbleModel::drawBRock() {
 	glPushMatrix();
 		glScalef(0.25f,0.2f,0.3f);
-        GLUquadric *quadratic=gluNewQuadric();
-		gluQuadricNormals(quadratic, GLU_SMOOTH);
-		gluSphere(quadratic,0.5,10,10);
-		gluDeleteQuadric(quadratic);
+        GLUquadric *qBRock=gluNewQuadric();
+		gluQuadricNormals(qBRock, GLU_SMOOTH);
+		glBindTexture(GL_TEXTURE_2D, TextureManager::getInstance()->getTextures("rusted_zinc.bmp"));
+		gluQuadricTexture(qBRock,true);
+
+		gluSphere(qBRock,0.5,10,10);
+		gluDeleteQuadric(qBRock);
 	glPopMatrix();
 	
 }
