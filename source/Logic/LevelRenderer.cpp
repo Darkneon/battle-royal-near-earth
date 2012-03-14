4aff4bd784240c75096ece9826a37a755c24328b
Jeffrey How <jeffrey.how@mail.mcgill.ca>
2012-03-13 21:18:52 -0400

    minor changes, class diagram, level renderer

--- source/Logic/LevelRenderer.cpp
+++ source/Logic/LevelRenderer.cpp
@@ -99,7 +99,9 @@ LevelRenderer::LevelRenderer() {
 	models[15]->removeAllChildren();
 	models[16]->removeAllChildren();
 	models[17]->removeAllChildren();
-	map1();   
+	//map1();   
+
+	level [15][15] = 18;
 
 } 
 
@@ -143,14 +145,14 @@ void LevelRenderer::render() {
 	glPopMatrix();
 	
 	//TO UNCOMMENT
-	/*for(int i = 1; i != 49; i++) {
-		for(int j = 23; j != 48; j++) {	
+	for(int i = 1; i != 49; i++) {
+		for(int j = 1; j != 48; j++) {	
 			glPushMatrix();
 				glTranslatef((GLfloat)i, 0.0f, (GLfloat)j);
 				models[0]->draw();
 			glPopMatrix();
 		}
-	}*/
+	}
 }
 
 void LevelRenderer::map1(){
